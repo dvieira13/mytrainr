8e10a5e38ef5d08f7cc4d7a6666de19d20823075
dvieira13 <vieiradylan13@gmail.com>
2025-05-06 15:35:46 -0400

    ojl

--- src/components/workout_types.ts
+++ src/components/workout_types.ts
@@ -5,11 +5,11 @@ $(".workout-icon-container").click(function () {
     const icon = $(this).find(".workout-toggle-icon");
     const icon_el = this.querySelector(".workout-toggle-icon") as HTMLElement;
     if ($(icon).hasClass("workout-open")) {
-        $(icon).attr("src", "./src/assets/icons/open.svg");
+        $(icon).attr("src", "https://iili.io/3OGwWcx.png");
         $(this).closest(".workout-inner").children(".caption-copy").hide();
     }
     else {
-        $(icon).attr("src", "./src/assets/icons/close.svg");
+        $(icon).attr("src", "https://iili.io/3OGkdDN.png");
         $(this).closest(".workout-inner").children().show();
     }
 
